edc90a41eb4f980b7d675848fce84d966d76aa2c
Thomas <thomascode92@hotmail.com>
2025-04-26 13:48:59 +0200

    Form Input State

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const title = ref("Tasks App");
+const newTask = ref("");
 </script>
 
 <template>
   <main>
-    <h1>{{ title }}</h1>
+    <h1>Tasks App</h1>
+    <form>
+      <label for="newTask">New Task</label>
+      <input id="newTask" v-model="newTask" name="newTask" />
+    </form>
+    <h3>{{ newTask }}</h3>
   </main>
 </template>
 
