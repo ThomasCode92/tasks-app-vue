98798cbe247e507838406b2426c2be8326d0c00e
Thomas <thomascode92@hotmail.com>
2025-04-27 14:06:59 +0200

    Form Validation & Reset

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -2,18 +2,31 @@
 import { ref } from "vue";
 
 const newTask = ref("");
+const error = ref("");
 
 const emit = defineEmits<{ addTask: [newTask: string] }>();
 
 function handleSubmit() {
-  emit("addTask", newTask.value);
+  const inputValue = newTask.value.trim();
+  if (inputValue) {
+    emit("addTask", inputValue);
+    newTask.value = "";
+  } else {
+    error.value = "Task cannot be empty";
+  }
 }
 </script>
 
 <template>
   <form @submit.prevent="handleSubmit">
     <label for="newTask">New Task</label>
-    <input id="newTask" v-model="newTask" name="newTask" />
+    <input
+      id="newTask"
+      v-model="newTask"
+      :aria-invalid="Boolean(error)"
+      @input="error = ''"
+    />
+    <small v-if="error" id="invalid-helper">{{ error }}</small>
     <div class="button-container">
       <button>Add</button>
     </div>
