8fd54ad1919c2197d1e3e9d856f131692953be93
Thomas <thomascode92@hotmail.com>
2025-05-01 09:53:10 +0200

    Emit Events instead of Changing Props

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@ function addTask(newTask: string) {
   const id = crypto.randomUUID();
   tasks.value.push({ id, title: newTask, done: false });
 }
+
+function toggleDone(id: string) {
+  const task = tasks.value.find(task => task.id === id);
+  if (task) task.done = !task.done;
+}
 </script>
 
 <template>
@@ -18,7 +23,7 @@ function addTask(newTask: string) {
     <TaskForm @add-task="addTask" />
     <h3 v-if="!tasks.length">Add a task to get started.</h3>
     <h3 v-else>0 / {{ tasks.length }} tasks completed.</h3>
-    <TaskList :tasks />
+    <TaskList :tasks @toggle-done="toggleDone" />
   </main>
 </template>
 
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -2,12 +2,18 @@
 import type { Task } from "../types";
 
 const props = defineProps<{ tasks: Task[] }>();
+const emits = defineEmits<{ toggleDone: [id: string] }>();
 </script>
 
 <template>
   <article v-for="task in props.tasks" :key="task.id" class="task-list">
-    <input :id="task.id + '-toggle'" v-model="task.done" type="checkbox" />
-    <label :for="task.id + '-toggle'">{{ task.title }}</label>
+    <input
+      :id="task.id + '-toggle'"
+      :checked="task.done"
+      type="checkbox"
+      @input="emits('toggleDone', task.id)"
+    />
+    <label :for="task.id + '-toggle'">{{ task.title }} </label>
   </article>
 </template>
 
